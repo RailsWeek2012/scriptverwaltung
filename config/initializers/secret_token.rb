9dbad13fc23b4579918eb415b136d6f937937348
Vincent Elliott Wagner <Vincent.Elliott.Wagner@mni.thm.de>
2012-07-19 13:30:14 +0200

    renew new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scriptverwaltung::Application.config.secret_token = 'b188897431fae21ef699e1ce389634f4e44a3c3e6f836c75b825b4ac7249ea4904f5b7e4d5602fe196231a964d8d1a88fa0a772e9b007286851014f4c9fd2979'
