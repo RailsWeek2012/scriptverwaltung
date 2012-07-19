b8ae11709188accba66c5e00f68ccacaf18f9f5a
Maf Zangana <maf.zangana@mni.thm.de>
2012-07-19 13:17:07 +0200

    delete all

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Scriptverwaltung::Application.config.secret_token = 'f57284d3fc835477f65c4a7482fc3e37d37f85bd484838b09ddac37730b1b04e3e36212851650083e72a95a7e2ef0fbe45d382b5534fc9e2e2f26ea7914bdda0'
