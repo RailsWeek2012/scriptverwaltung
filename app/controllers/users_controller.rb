ccb7a72870f9f54593b6d54d85e77fb75091b447
Vincent Elliott Wagner <Vincent.Elliott.Wagner@mni.thm.de>
2012-07-25 21:08:59 +0200

    now only admins could give admin rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_filter :isAdmin?, :only => [:toggle_admin]
+	before_filter :admin?, :only => [ :toggle_admin ]
+  before_filter :require_login!, :except => [ :show ]
   def show
     @user = User.find(params[:id])
     store_return_url
@@ -7,6 +8,13 @@ class UsersController < ApplicationController
 
   def toggle_admin
   	User.find(params[:id]).toggle(:isAdmin).save
-  	redirect_to show_user_path current_user
+  	redirect_to show_user_path(current_user)
   end
+
+  private
+    def admin?
+      unless isAdmin?
+        redirect_to scripts_path, alert: "Sie haben keine Berechtigung zum Aufruf dieser Ressource "
+      end
+    end
 end
